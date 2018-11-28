8269ed66e705c546c48aaafa191451a6be752c41
Roland Kammerer <roland.kammerer@linbit.com>
2018-11-28 16:06:36 +0100

    add custom nerdtree 'yy'

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf ../../.vimrc
-ln -s $PWD/vimrc ~/.vimrc
+ln -s "$PWD"/vimrc ~/.vimrc
 
 rm -rf ../bundle
 mkdir -p ../bundle
@@ -10,5 +10,5 @@ if [ "$1" = "youcompleteme" ]; then
 	vam install -t ./youcompleteme.vim.vam youcompleteme
 fi
 
-cd ../bundle
-find ../bundles -maxdepth 1 -type d -name "*.vim.*" -exec ln -s {} \;
+(cd .. && ln -s ./bundles/nerdtree_plugin)
+(cd ../bundle && find ../bundles -maxdepth 1 -type d -name "*.vim.*" -exec ln -s {} \;)
